df17ca42a005b9fbe751701c21b69da9c3ba4f21
Moazzam Waheed <moazzamwaheed@gmail.com>
2023-01-15 02:45:20 +0500

    topcoder tensorflow challenge

---
+++ main.py
@@ -0,0 +1,8 @@
+from RPS_game import play, mrugesh, abbey, quincy, kris, human, random_player
+from RPS import player
+from unittest import main
+
+play(player, quincy, 1000, verbose=True)
+#play(player, abbey, 1000, verbose=False)
+#play(player, kris, 1000, verbose=False)
+#play(player, mrugesh, 1000, verbose=False)
